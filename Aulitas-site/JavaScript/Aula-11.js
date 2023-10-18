1ec6983fafa3ca16e27380d717f7ee132e97c25f
Gusthavo Gonçalves <gusthavo.goncalves@gmail.com>
2023-10-18 18:13:11 -0300

    umas coisas com fetch, texto, Imagem, array.

---
+++ Aulitas-site/JavaScript/Aula-11.js
@@ -0,0 +1,69 @@
+xhr = new XMLHttpRequest();
+console.log(xhr);
+
+// xhr.onreadystatechange = function(){
+//       if(xhr.readyState == 4 && xhr.status == 200){
+//             let response = JSON.parse(xhr.responseText);
+//             console.log(response);
+//       }
+// }
+
+// xhr.open("GET","/locadoraCarros.Json",true)
+// xhr.send();
+catchJujuba()
+.then(response =>{
+    console.log("foi jujuba")
+})
+.catch(ERROR =>{
+    console.log(ERROR);
+    console.log("ERROR");
+});
+
+async function catchJujuba(){
+    const response = await fetch("/Aulitas-site/Image/juju.png");
+    const blob = await response.blob();
+    document.getElementById("jujuba").src = URL.createObjectURL(blob);
+}
+
+async function catchTexto(){
+    const response = await fetch("/Cores.txt");
+    const texto = await response.text();
+    document.getElementById("texto").innerText = texto; 
+}
+
+catchTexto()
+.then(response =>{
+    console.log("foi texto")
+})
+.catch(ERROR =>{
+    console.log(ERROR);
+    console.log("ERROR");
+});
+
+
+const filenames = [
+    "/Aulitas-site/Image/juju.png",
+    "/Aulitas-site/Image/todynho.png"
+]
+async function catchImagens(filenames){
+    for(let filename of filenames){
+        const response = await fetch(filename);
+        const blob = await response.blob();
+        const img = document.createElement('img');
+        img.src = URL.createObjectURL(blob);
+        img.width = "250";
+        document.getElementById("images").append(img);
+  
+    }
+}
+catchImagens(filenames)
+.then(response =>{
+    console.log("foi imagens")
+})
+.catch(ERROR =>{
+    console.log(ERROR);
+    console.log("ERROR");
+});
+
+
+
